11e42150619988a020c79f4c241dcf427e5b12cd
Sumit <sumitu27@gmail.com>
2020-05-20 09:42:38 +0530

    Enhanced Application

--- server.py
+++ server.py
@@ -15,7 +15,7 @@ CORS(app)
 def upload_file():
     return render_template('index.html')
 
-@app.route('/add_face', methods=['GET', 'POST'])
+@app.route('/dev/add_face', methods=['GET', 'POST'])
 def add_face_in_collection():
 
     f = request.files["file"]
@@ -27,10 +27,10 @@ def add_face_in_collection():
 
     add_faces_to_collection('my-personal-data-sumit', os.path.basename(upload_file_name), "family_collection")
 
-    return "Face Added Successfully"
+    return "Face Added Successfully :::: " + str(file_name)
 
 
-@app.route('/list_of_faces', methods=['GET', 'POST'])
+@app.route('/dev/list_of_faces', methods=['GET', 'POST'])
 def list_of_faces():
     collection_id = 'family_collection'
 
@@ -39,7 +39,7 @@ def list_of_faces():
     return jsonify(faces_count)
 
 
-@app.route('/detect_faces', methods=['GET', 'POST'])
+@app.route('/dev/detect_faces', methods=['GET', 'POST'])
 def detect_faces():
 
     f = request.files["file"]
@@ -51,24 +51,29 @@ def detect_faces():
 
     return_response = detect_faces_in_image(os.path.basename(upload_file_name), 'my-personal-data-sumit', upload_file_name)
 
-    return jsonify(return_response)
+    return_dict ={}
+    return_dict["Person Identified"] = return_response
 
-@app.route('/create_face_collection', methods=['GET', 'POST'])
+    return jsonify(return_dict)
+
+@app.route('/dev/create_face_collection', methods=['GET', 'POST'])
 def create_face_collection():
 
     collection_id = 'family_collection'
     status_code = create_collection(collection_id)
     print('Status code: ' + str(status_code))
-
+    if str(status_code).strip() == "200":
+        return "Face Collection Created Successfully"
     return str(status_code)
 
-@app.route('/delete_face_collection', methods=['GET', 'POST'])
+@app.route('/dev/delete_face_collection', methods=['GET', 'POST'])
 def delete_face_collection():
 
     collection_id = 'family_collection'
     status_code = delete_collection(collection_id)
     print('Status code: ' + str(status_code))
-
+    if str(status_code).strip() == "200":
+        return "Face Collection Deleted Successfully"
     return str(status_code)
 
 if __name__ == '__main__':
